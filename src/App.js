fdb14e47dcbfeb7df724bc29eb47e5006681e664
Ted.Nguyen <nvkhiem2016@gmail.com>
2021-03-24 23:55:14 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Khiem NV
+          Hello Khiem NV Dep Trai
         </p>
         <a
           className="App-link"
