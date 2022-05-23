aae9abda6c5c8d46bed4857adf8a46f310326d68
nvkhiem2016 <41146998+nvkhiem2016@users.noreply.github.com>
2022-05-23 11:33:19 +0700

    version 2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dev Branch
+          Dev Branch Version 2
         </a>
       </header>
     </div>
