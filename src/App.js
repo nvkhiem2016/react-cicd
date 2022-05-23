5e69c3aec548153c297dbce929fd36d4a236f7ea
nvkhiem2016 <41146998+nvkhiem2016@users.noreply.github.com>
2022-05-23 11:56:00 +0700

    Revert "edit v4"

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dev Branch Version 4
+          Dev Branch Version 3
         </a>
       </header>
     </div>
