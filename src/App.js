219f4ea2aeeb9737c741e91cb2c4188616a39f55
Ted.Nguyen <nvkhiem2016@gmail.com>
2022-05-26 09:53:34 +0700

    productionproduction

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          github.event.pull_request.merged == 'true' production
+          github.event.pull_request.merged == 'true' production production production production
         </a>
       </header>
     </div>
